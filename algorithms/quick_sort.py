f597b5082503708bcdf296196685a9c8b4d98f9b
katmutua <jkm.mutua@gmail.com>
2015-03-17 10:07:36 +0300

    change upper array and lower array fetch for quicksort

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -3,6 +3,12 @@ class QuickSort:
     def __init__(self, list_a):
         self.list_a = list_a
 
+    def sort_values(self):
+        return 'nothing yet'
+
+    def pivot_element(self):
+        return self.list_a[self.list_length() - 1]
+
     def list_length(self):
         return len(self.list_a)
 
@@ -13,10 +19,12 @@ class QuickSort:
         elif self.list_length() == 1:
             return self.list_a
         else:
-            return self.list_a[:self.list_length()/2]
+            lower_list = [i for i in self.list_a if i<= self.pivot_element()]
+            return lower_list
 
     def upper_array(self):
-        return self.list_a[self.list_length()/2:]
+        upper_list = [i for i in self.list_a if i not in self.lower_array()]
+        return upper_list
 
 
 quick_sort = QuickSort([2,4,5,3,1,9,4,5,10])
